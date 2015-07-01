ea625f9eea36fbc9eaaf1715178eae1861a60729
Cristian Cisternas <Cristian Cisternas@Cristian>
2015-07-01 19:04:53 -0300

    pequeñas mejoras

--- application/views/content.php
+++ application/views/content.php
@@ -3,8 +3,8 @@
 <div id="login" hidden>
     <div style="font-size: 20px;"><h1>Inicio de Sesión</h1></div>
     <hr style="width: 35%;"><br>
-    <input class="user_icon" placeholder="Usuario" size="30" id="user" maxlength="30" style="font-size: 20px;" required onkeypress="enter_user(event)" autofocus/><br>
-    <input class="pass_icon" placeholder="Contraseña" type="password" size="30" id="pass" style="font-size: 20px;" required onkeypress="enter_conectar(event)"/><br>
+    <input class="user_icon" placeholder="Usuario" size="30" id="user" maxlength="30" required onkeypress="enter_user(event)" autofocus/><br>
+    <input class="pass_icon" placeholder="Contraseña" type="password" size="30" id="pass" required onkeypress="enter_conectar(event)"/><br>
     <br>
     <button id="conectar">Conectar</button>
     <hr style="width: 35%;"><br>
